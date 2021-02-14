396d840b39ba72ddc5c6494034f65bf36073a880
akakss225 <akakss225@naver.com>
2021-02-14 19:25:14 +0900

    Codeup17

---
+++ Basic2/src/Num17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Num17 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		
+		StringTokenizer token = new StringTokenizer(a,": ");
+		
+		int[] arr = new int[2];
+		
+		int i = 0;
+		
+		while(token.hasMoreTokens()) {
+			arr[i] = Integer.parseInt(token.nextToken());
+			i += i;
+		}
+		
+		String hour = String.format("%d", arr[0]);
+		String min = String.format("%d", arr[1]);
+		
+		System.out.println(hour + ":" + min);
+		
+		sc.close();
+		
+
+	}
+
+}

aca8452b280ec71fe02f25a642ffb509973b13c6
akakss225 <akakss225@naver.com>
2021-02-14 18:30:22 +0900

    Codeup13

--- Basic2/src/Num13.java
+++ Basic2/src/Num13.java
@@ -1,8 +1,15 @@
 
+
 public class Num13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		int a,b;
+		a = 1;
+		b = 2;
+		
+		System.out.printf("%d %d", a,b);
 
 	}
 

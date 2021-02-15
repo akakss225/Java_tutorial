4a0ebca26cffb4fdce6dfcbf94b37960605f9727
akakss225 <akakss225@naver.com>
2021-02-15 18:43:13 +0900

    Codeup23

--- Basic2/src/Num22.java
+++ Basic2/src/Num22.java
@@ -24,7 +24,7 @@ public class Num22 {
 		
 		
 		
-		
+		sc.close();
 
 	}
 
---
+++ Basic2/src/Num23.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Num23 {
+
+	public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        
+	        String text = sc.nextLine();
+	        
+	        char total[] = text.toCharArray();
+	        
+	        for(int i=0; i<total.length; i++) {
+	            System.out.println("'"+total[i]+"'");
+	        }
+	    
+	}
+
+}

73a81b16cd6ace17b3b0de452bb3187e7e963926
akakss225 <akakss225@naver.com>
2021-02-16 19:03:56 +0900

    Codeup33

---
+++ Basic2/src/Num33.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Num33 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String num = sc.nextLine();
+		
+		System.out.println(Integer.parseInt(num, 8));
+	
+
+	}
+
+}

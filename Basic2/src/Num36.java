56578fa119c5dd91a97df76f6523c92bd1cc614f
akakss225 <akakss225@naver.com>
2021-02-17 18:29:58 +0900

    Codeup36

---
+++ Basic2/src/Num36.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Num36 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.println((char)num);
+
+	}
+
+}

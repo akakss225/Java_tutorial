40229d72c94b545ee2ee2a56c32fccd18a2029a8
akakss225 <akakss225@naver.com>
2021-02-17 18:20:21 +0900

    Codeup35

---
+++ Basic2/src/Num35.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Num35 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        char word = sc.next().charAt(0);
+        
+        System.out.println((int)word);
+        
+    }
+}

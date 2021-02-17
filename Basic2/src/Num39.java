70bc35b82e8e5a63f96712695ba7a789ff962f5e
akakss225 <akakss225@naver.com>
2021-02-17 19:09:05 +0900

    Codeup39

---
+++ Basic2/src/Num39.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Num39 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		System.out.println(-a);
+
+	}
+
+}

9f657e9905bb6305036f1b96956b7511b562b963
akakss225 <akakss225@naver.com>
2021-02-17 19:15:59 +0900

    Codeup40

---
+++ Basic2/src/Num40.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Num40 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		char a = sc.next().charAt(0);
+		
+		System.out.print((char)(a+1));
+		
+		
+		
+ 
+	}
+
+}

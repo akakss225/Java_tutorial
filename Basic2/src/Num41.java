5f26dd8fa9787f4f870a612a4599c609f8d78f61
akakss225 <akakss225@naver.com>
2021-02-17 19:25:30 +0900

    Codeup41

---
+++ Basic2/src/Num41.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Num41 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String num = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(num);
+		
+		int[] arr = new int[2];
+		int i = 0;
+		
+		while(st.hasMoreTokens())
+		{
+			arr[i] = Integer.parseInt(st.nextToken());
+			i += 1;
+		}
+		
+		if(arr[1] != 0)
+		{
+			System.out.println(arr[0]/arr[1]);
+		}
+
+	}
+
+}

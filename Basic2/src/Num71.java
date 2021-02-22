ecbf354e49805db2536d666816e8fe167dc08ff4
akakss225 <akakss225@naver.com>
2021-02-22 18:41:28 +0900

    Codeup71

---
+++ Basic2/src/Num71.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Num71 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(true)
+		{
+			int a = sc.nextInt();
+		
+		
+		if(a == 0)
+		{
+			break;
+		}
+		else
+		{
+			System.out.println(a);
+		}
+		}
+
+	}
+
+}

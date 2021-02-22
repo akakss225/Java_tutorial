02c3c3929b572561fb1acf3b5c23673a1ec44b13
akakss225 <akakss225@naver.com>
2021-02-22 17:21:53 +0900

    Codeup61

---
+++ Basic2/src/Num61.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Num61 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(s);
+		
+		int[] arr = new int[2];
+		
+		int i = 0;
+		
+		while(st.hasMoreTokens())
+		{
+			arr[i] = Integer.parseInt(st.nextToken());
+			i += 1;
+		}
+		
+		System.out.println(arr[0]|arr[1]);
+
+	}
+
+}

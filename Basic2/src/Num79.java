0150c3acafa27dc85615166f327ca895fb699fb1
akakss225 <akakss225@naver.com>
2021-02-22 19:40:36 +0900

    Codeup79

---
+++ Basic2/src/Num79.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Num79 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		while(true)
+		{
+			char x = sc.next().charAt(0);
+			if(x == 'q')
+			{
+				System.out.println(x);
+				break;
+			}
+			else
+			{
+				System.out.println(x);
+			}
+		}
+		
+
+	}
+
+}

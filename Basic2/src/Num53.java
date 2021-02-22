f64ba41d3042dce3244b619a804bb36693be90ab
akakss225 <akakss225@naver.com>
2021-02-22 16:40:29 +0900

    Codeup54

--- Basic2/src/Num53.java
+++ Basic2/src/Num53.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class Num53 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num == 1)
+		{
+			System.out.println(0);
+		}
+		else if(num == 0)
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			System.out.println("1 또는 0만을 입력해 주세요");
+		}
+		
+
 
 	}
 
---
+++ Basic2/src/Num54.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Num54 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(a);
+		
+		int[] arr = new int[2];
+		int i = 0;
+		
+		while(st.hasMoreTokens())
+		{
+			arr[i] = Integer.parseInt(st.nextToken());
+			i += 1;
+		}
+		
+		if(arr[0] == 1 && arr[1] == 1)
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			System.out.println(0);
+		}
+				
+			
+				
+
+	}
+
+}

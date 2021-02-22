32a7f9c93a961a06f368ffb6257615c9b2d5dc35
akakss225 <akakss225@naver.com>
2021-02-22 18:04:38 +0900

    Codeup68

---
+++ Basic2/src/Num68.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Num68 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int score = sc.nextInt();
+		
+		if(score >= 90)
+		{
+			System.out.println("A");
+		}
+		else if(score >= 70)
+		{
+			System.out.println("B");
+		}
+		else if(score >= 70)
+		{
+			System.out.println("C");
+		}
+		else
+		{
+			System.out.println("D");
+		}
+
+	}
+
+}

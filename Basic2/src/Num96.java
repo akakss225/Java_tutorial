b3d5008995b309ecfe9b823b551236444b6a4b29
akakss225 <akakss225@naver.com>
2021-02-25 19:51:35 +0900

    Codeup96

---
+++ Basic2/src/Num96.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Num96 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[20][20];
+		
+		int a = sc.nextInt();	
+		
+		int x,y;
+		
+	
+		
+		for(int i = 0; i < a; i++)
+		{
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+			arr[x-1][y-1] = 1;
+		}
+		
+		for(int k = 0; k < 20; k++ )
+		{
+			for(int l = 0; l < 20; l++)
+			{
+				System.out.print(arr[k][l] + " ");
+			}
+			System.out.println();
+		}
+		
+
+		
+
+	}
+
+}

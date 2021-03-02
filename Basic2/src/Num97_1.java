cd072c31895babfa87d7f1b5bead20d406fd131c
akakss225 <akakss225@naver.com>
2021-03-02 15:44:11 +0900

    자바 기초 Polymorphism5

--- Basic2/src/Num97_1.java
+++ Basic2/src/Num97_1.java
@@ -1,9 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class Num97_1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Num97_1{
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        try {
+        String[][] arr = new String[19][19];
+        
+       
+        for(int i=0; i<arr.length; i++) {
+            
+            String[] ar1 = br.readLine().split(" ");
+            for(int j=0; j<arr.length; j++) {
+      
+                arr[i][j] = ar1[j];
+            }
+        }
+        
+       
+        int n = Integer.parseInt(br.readLine());  
+        
+        ArrayList<String> list = new ArrayList();		
+        
+        
+        for (int i = 0; i < n; i++) {
+            list.add(br.readLine());
+            
+        }
+        System.out.println(list);
+        
+        for (int i = 0; i < list.size(); i++) {
+            
+            
+            String[] point = list.get(i).split(" ");
+            
+            int x = Integer.parseInt(point[0]);
+            int y = Integer.parseInt(point[1]);
+            
+            for (int j = 0; j < 19; j++) {
+                if(arr[x-1][j] == "0"){ 
+                    arr[x-1][j] = 1 + ""; 
+                }else{
+                    arr[x-1][j] = 0 + ""; 
+                }
+            }
+            for (int j = 0; j < 19; j++) {
+                if(arr[j][y-1] == "0"){
+                    arr[j][y-1] = 1 + ""; 
+                }else{
+                    arr[j][y-1] = 0 + ""; 
+                }
+            }
+        }
+        
+        
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        
+        }catch(NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- 객체지향/src/다형성/PolymorphismDemo4.java
+++ 객체지향/src/다형성/PolymorphismDemo4.java
@@ -44,6 +44,11 @@ public class PolymorphismDemo4 {
 
 	public static void main(String[] args) {
 
+		/*이때 굳이 데이터타입과 인스턴스를 다르게 하는 이유는
+		 * 위에서 볼 수 있듯, 서로 다른 메소드를 출력해야하는 상황에 반하여
+		 * 출력하는 방식은 동일하게 통일 시켜줄 수 있다는 장점이 있기 때문이다.
+		 * execute메소드를 실행 할 때 코드를 굳이 plus와 minus로 나누지 않아도 된다는 장점이 존재한다.
+		 */
 		Calculator c1 = new CalculatorDecoPlus();
 		c1.setOprands(10, 20);
 		

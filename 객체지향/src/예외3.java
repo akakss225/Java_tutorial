177d103b5629989d4b8bc25e4c578187777b8d9b
akakss225 <akakss225@naver.com>
2021-03-03 16:39:34 +0900

    자바 기초 Exception3

---
+++ 객체지향/src/예외3.java
@@ -0,0 +1,32 @@
+class A{
+	private int[] arr = new int[3];
+	A() {
+		arr[0] = 0;
+		arr[1] = 10;
+		arr[2] = 20;
+	}
+	//한가지 입력에서 두가지의 에러가 발생할 수 있음.
+	public void z(int first, int second) {
+		try {
+		System.out.println(arr[first] / arr[second]);
+		}catch(ArithmeticException e) {
+			System.out.println("ArithmeticException e");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException e");
+			
+		}
+	}
+}
+
+
+public class 예외3 {
+
+	public static void main(String[] args) {
+
+		A a = new A();
+		
+		a.z(1, 0);
+
+	}
+
+}

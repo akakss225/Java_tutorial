fc37a352d9d250ee6a3ea2116de6ae03954ecd7d
akakss225 <akakss225@naver.com>
2021-03-03 15:19:39 +0900

    Programming Class1

---
+++ Programing Class(Python)/src/FirstWeek/첫주차.java
@@ -0,0 +1,20 @@
+package FirstWeek;
+
+public class 첫주차 {
+
+	public static void main(String[] args) {
+		
+		long A = 7995868;
+		long B = 4474694;
+
+		System.out.println("A+B를 구하시오 : " + A + B);
+		System.out.println("A*B를 구하시오 : " + (A * B));
+		System.out.println("A-B를 구하시오 : " + (A - B));
+		System.out.println("A/B를 구하시오 : " + (A / B));
+		System.out.println("A%B를 구하시오 : " + (A % B));
+		System.out.println("A^5를 구하시오 : " + Math.pow(A, 5));
+		
+
+	}
+
+}

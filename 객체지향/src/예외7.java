244cd2f0d2a9ad40e67452b60c39a4b18b664a85
akakss225 <akakss225@naver.com>
2021-03-04 17:43:43 +0900

    자바 기초 Exception8

--- 객체지향/src/예외7.java
+++ 객체지향/src/예외7.java
@@ -1,8 +1,38 @@
+import java.io.IOException;
+
+/* 예외의 종류 
+ * illegalArgumentException : 매개변수가 의도하지 않은 상황을 유발시킬 때
+ * IllegalStateException : 메소드를 호출하기 위한 생타개 아닐 때
+ * NullPointerException : 매개 변수 값이 null일 때
+ * IndexOutOfBoundsException : 인덱스 매개 변수 값이 범위를 벗어날 때
+ * ArithmeticException : 산술적인 연산에 오류가 있을 때
+ */
+
+class E{
+	void thorwArithmeticException() {
+		throw new ArithmeticException();
+	}
+	
+	
+	// 1. try catch로 잡기
+	void throwIOException1() {
+		try {
+			throw new IOException();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// 2. 사용자에게 던지기
+	void throwIOException2() throws IOException {
+		throw new IOException();
+	}
+}
 
 public class 예외7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
 
 	}
 
---
+++ 객체지향/src/예외8.java
@@ -0,0 +1,48 @@
+	/* IOException은 ArithmeticException과 다르게 반드시 예외처리를 해야한다.
+	* ArithmeticException는 부모중에 RuntimeException이 존재한다.
+	* 이러한 RuntimeException이 없는 IOException같은 예외를
+	* Checked Exception이라고하고, 반대는 Unchecked Exception이라고 한다.
+	* Checked Exception의 경우 반드시 try catch 혹은 throws를 이용해
+	* 예외처리를 해주어야한다.
+	* 이를 이용하여 나만의 Exception을 만들 수 있다.
+	*/
+//이때 부모class를 Exception 혹은 IOException으로 해주면, checked Exception이기 때문에, 반드시 예외처리를 해주어야한다.
+class DivideException extends ArithmeticException{
+	//기본 생성자는 존재하지만,
+	DivideException(){
+		super();
+	}
+	//기본 생성자가 아니면, 직접 만들어주어야 한다.
+	DivideException(String message){
+		super(message);
+	}
+}
+class Cal3{
+	int left,right;
+	public void set(int left, int right) {
+		
+		this.left = left;
+		this.right = right;
+	}
+	public void devide() {
+		
+		if(right == 0) {
+			throw new DivideException("0으로 나눌 수 없습니다.");
+		}			
+		System.out.print(this.left/this.right);
+		}
+	}
+
+
+
+public class 예외8 {
+
+	public static void main(String[] args) {
+
+		Cal3 c1 = new Cal3();
+		c1.set(10, 0);
+		c1.devide();
+
+	}
+
+}

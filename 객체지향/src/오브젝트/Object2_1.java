09afc2c417efbbb4f5257eec21ba774479d9078e
akakss225 <akakss225@naver.com>
2021-03-05 17:13:05 +0900

    자바 기초 Object2_1

--- 객체지향/src/오브젝트/Object2_1.java
+++ 객체지향/src/오브젝트/Object2_1.java
@@ -1,6 +1,6 @@
 package 오브젝트;
 
-class Calculator{
+class Calculator1{
 	int left, right;
 	
 	public void set(int left, int right) {
@@ -13,19 +13,20 @@ class Calculator{
 	public void avg() {
 		System.out.println((this.left + this.right) / 2);
 	}
+	
+	//toString 오브젝트를 오버라이딩 할 수 있다.
+	public String toString() {
+		return "left : " + this.left + ", right : " + this.right;
+	}
 }
 
-public class Object2 {
+public class Object2_1 {
 
 	public static void main(String[] args) {
 
-		Calculator c = new Calculator();
+		Calculator1 c = new Calculator1();
 		
 		c.set(10, 20);
-		
-		//그냥 매개변수인 c만 입력하는것과 toString은 같은 출력값을 나타낸다.
-		//즉, 오브젝트 클래스인 toString은 생략이 된 상태이지만, 그 기능을 수행한다.
-		//여기서 toString에 대하여, 사용자가 오버라이딩을 통해 원하는 오브젝트를 수정 할 수 있다.
 		
 		System.out.println(c.toString());
 

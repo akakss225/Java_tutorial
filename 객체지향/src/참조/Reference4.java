480950c76206884b6466c9e1550a04443cf66567
akakss225 <akakss225@naver.com>
2021-03-10 18:54:46 +0900

    자바 기초 ArrayList

--- 객체지향/src/참조/Reference4.java
+++ 객체지향/src/참조/Reference4.java
@@ -10,6 +10,8 @@ public class Reference4 {
 		_value(a);
 		System.out.println("runValue, " + a);
 	}
+	
+	
 	static void _reference1(A b) {
 		b = new A(2);
 	}
@@ -18,6 +20,10 @@ public class Reference4 {
 		_reference1(a);
 		System.out.println("runReference1, " + a.id);
 	}
+	//결국  A를 참조하는 a의 경우 1 을 담고있고, 새로은 A를 참조하는 b의 경우 2를 담는것을 의미한다. 
+	//즉, 다른 주소를 사용하기 떄문에 a를 출력하면 1 이 나온다.
+	
+	
 	static void _reference2(A b) {
 		b.id = 2;
 		
@@ -27,6 +33,10 @@ public class Reference4 {
 		_reference2(a);
 		System.out.println("runReference2, " + a.id);
 	}
+	/* 여기서는 A를 참조하는 a의 경우 ,1을 받지만, b.id = 2 라는 값을 받게 되면, 
+	 * 같은 A를 참조하는 b의 값이 바뀌기 때문에, 같은 주소를 사용하여 바꾼것이므로, a값 또한 바뀌게 된다.
+	 */
+			
 
 	public static void main(String[] args) {
 
